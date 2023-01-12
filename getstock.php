e01a390e33aca5983dcfec8b237c3b152eab2295
terzettong <88244868+terzettong@users.noreply.github.com>
2023-01-12 12:33:01 +0100

    Update getstock.php

--- getstock.php
+++ getstock.php
@@ -1,14 +1,11 @@
 <?php
-    $mysqli = require __DIR__ . "/database.php";
 
 $id = $_GET['type_id'];
 // $data = null;
 $year_id = $_GET['year_id'];
 
 
-$getData = mysqli_query($con, "SELECT count(*) as maxNo FROM account_details WHERE service ='{$id}' AND year ='{$year_id}'   ");
-$query = mysqli_fetch_assoc($getData);
-$userNo = $query['maxNo'];
+
 
 $love='I LOVE YOU';
-echo $userNo;
+echo $id;
