5fae9ea11ee61562160913668043922edac0c071
terzettong <88244868+terzettong@users.noreply.github.com>
2023-01-12 12:31:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,107 @@
+const generateSelect = (target) => {
+
+    //add custom select structure
+    function wrap(el, classes, ids) {
+        let wrapper = document.createElement( 'div' );
+        let innerWrapper = document.createElement( 'ul' );
+        let label = document.createElement( 'div' );
+        el.parentNode.insertBefore(wrapper, el);
+        let classList = classes.split(' ').filter(v => v !== '')
+        wrapper.classList.add( ...classList );
+        wrapper.setAttribute('id', ids);
+        innerWrapper.classList.add( 'options' );
+        label.classList.add( 'label' );
+        wrapper.appendChild(el);
+        wrapper.appendChild(label);
+        wrapper.appendChild(innerWrapper);
+    }
+
+    //get all select elements
+    const select = document.querySelectorAll( target );
+    for(e of select){
+
+        //get select classes
+        const classes = e.classList.value;
+        const ids = e.getAttribute('id');
+
+        // add wrapper in select element
+        wrap(e, `select ${classes}`, ids);
+
+        // add custom select options
+        const options = e.querySelectorAll( 'option' );
+        for(let i = 0; i < options.length; i++){
+            let html = `<li onClick="getStock2()"  class='${options[i].selected && 'current'}' value='${options[i].value}'>${options[i].innerText}</li>`;
+            e.parentNode.querySelector( '.options' ).innerHTML += html;
+        }
+
+        //add options and label
+        e.parentNode.querySelector( '.label' ).innerHTML = options[0].innerHTML;
+
+    }
+
+    //change label value when user select option
+    let selectLabel =  document.querySelectorAll('.select .label');
+    selectLabel.forEach(e => {
+        e.addEventListener('click', () => {
+            if(e.parentNode.classList.contains('active')){
+                e.parentNode.classList.remove('active');
+            }else{
+                e.parentNode.classList.add('active');
+            }
+        });
+    });
+
+    //change select value when user select option
+    let selectOptions =  document.querySelectorAll('.select .options > li');
+    selectOptions.forEach(e => {
+        e.addEventListener('click', () => {
+            e.closest(".select").querySelector('.label').innerHTML = e.innerHTML;
+            e.closest(".select").classList.remove('active');
+            e.closest(".select").querySelector('select').value = e.getAttribute("value");
+            let siblings = e.closest(".select").querySelectorAll('.options li')
+            for(s of siblings){
+                s.classList.remove('current')
+            }
+            e.classList.add('current')
+        });
+    });
+
+    //remove active class when user click outside
+    let outsideSelect =  document.querySelectorAll('.select');
+    outsideSelect.forEach(elm => {
+        document.addEventListener('click', (e) => {
+            if(!elm.contains(e.target)){
+                elm.classList.remove('active')
+            }
+        })
+    })
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+   generateSelect('select');
+})
+function getStock2(e) {
+    console.log(e)
+    let type_id = $('#acctype').val();
+    let year_id = $('#accyear').val();
+    let btn = $('#stockBtn');
+    
+    if (type_id !== '' && year_id !== '') {
+        $.ajax({
+            url: 'getstock.php',
+            method: 'GET',
+            beforeSend() {
+                btn.html('<i class="fa fa-spin fa-spinner"></i>');
+            },
+            data: {
+                type_id: type_id,
+                year_id: year_id,
+            },
+        }).done((res) => {
+            btn.html(`Stock: <span class="charge_number">${res}</span>`);
+        }).fail(() => {
+            console.log('something went wrong');
+        })
+    }
+}
---
+++ select.php
@@ -0,0 +1,87 @@
+<?php
+    $mysqli = require __DIR__ . "/database.php";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $id = $_POST['acctype'];
+     $year_id = $_POST['accyear'];
+$qry2 = "SELECT * FROM account_details WHERE service ='{$id}' AND year ='{$year_id}'";
+$log = mysqli_query($con, $qry2);
+
+    if (mysqli_num_rows($log) > 0)
+    
+    {
+        
+
+$file = 'record.txt';
+$person = "Already Submitted for '$id'\n";
+
+
+file_put_contents($file, $person, FILE_APPEND | LOCK_EX);
+    }
+}
+?>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script type="text/javascript" src="script.js"> </script>
+<link rel="stylesheet" href="style.css">
+
+</head>
+<form action="" method="POST">
+<div class="first">
+<label>Service</label>
+
+<select name="acctype"  id="acctype" onChange="getStock()" required>
+<option value="instagram">Instagram</option>
+<option value="twitter">Bro</option>
+<option value="discord">Hello</option>
+
+</select>
+</div>
+<div class="second">
+    <label>Year</label>
+<select name="accyear" id="accyear" onChange="getStock()" required>
+<option value="2022">2022</option>
+<option value="2016">2016</option>
+<option value="2016">2016</option>
+
+</select>
+</div>
+<p>
+<div class="third">
+<p class="btn btn-secondary total_charge" name="stockBtn" id="stockBtn">Stock: <span name="stockBtn" class="charge_number">0</span></p>
+</div>
+
+<buttontype="submit" class="btn round btn-primary btn-min-width mr-1 mb-1">Submit</button>
+</form>
+<script>
+        function getStock() {
+            let type_id = $('#acctype').val();
+            let year_id = $('#accyear').val();
+            let btn = $('#stockBtn');
+            
+            if (type_id !== '' && year_id !== '') {
+                $.ajax({
+                    url: 'getstock.php',
+                    method: 'GET',
+                    beforeSend() {
+                        btn.html('<i class="fa fa-spin fa-spinner"></i>');
+                    },
+                    data: {
+                        type_id: type_id,
+                        year_id: year_id,
+                    },
+                }).done((res) => {
+                    btn.html(`Stock: <span class="charge_number">${res}</span>`);
+                }).fail(() => {
+                    console.log('something went wrong');
+                })
+            }
+        }
+      </script>
+
